7057b1ca034c10b70cf61d7fa26697de5fa5e187
Davi Castilho <davi.lc@sempreceub.com>
2025-06-18 09:21:24 -0300

    Update Script biblioteca prova final.sql

--- Script biblioteca prova final.sql
+++ Script biblioteca prova final.sql
@@ -274,3 +274,30 @@ values
 (38, 33, 32, '2023-01-06', 'ativa', 'não'),
 (39, 16, 28, '2023-07-19', 'ativa', 'sim'),
 (40, 29, 37, '2024-01-22', 'concluída', 'não');
+
+-- 1) Para cada autor, exiba o total de livros cadastrados e quantos estão disponíveis atualmente. Mostre apenas os autores com mais de 1 livro no sistema.
+Select a.nome,count(l.id) as total_livros,sum(disponibilidade) as livros_disponiveis from Autores as a join Livros as l on a.id = l.autor_id group by a.nome having count(l.id) > 1;
+
+-- 2) Liste o nome dos usuários e a quantidade de livros que cada um já pegou emprestado. Mostre apenas os usuários que já realizaram pelo menos 2 empréstimos, ordenando do que mais emprestou para o que menos emprestou.  
+select nome,count(id_usuario) from usuarios as u left join Emprestimos as e on u.id = e.id_usuario group by nome having count(id_usuario) >= 2 order by nome desc;
+
+-- 3) Classifique os usuários como "Alta", "Média" ou "Baixa" atividade com base na quantidade de empréstimos:
+-- 10 ou mais: Alta
+-- Entre 5 e 9: Média
+-- Abaixo de 5: Baixa
+select nome,count(id_usuario), 
+case
+when count(id_usuario) >= 10 then 'Alta'
+when count(id_usuario) between 5 and 9 then 'Média'
+when count(id_usuario) <5 then 'Baixa'
+end as atividade
+from usuarios as u left join Emprestimos  as e on u.id = e.id_usuario group by nome;
+
+-- 4) Mostre quantos usuários se cadastraram em cada mês. 
+select DATE_FORMAT(data_cadastro, '%m') as mes, count(id) as Qtd_cadastrados from Usuarios group by mes order by mes;
+
+
+-- 5) Mostre os usuários que mais pegaram livros emprestados, juntamente com o total de empréstimos por usuário, ordenados do maior para o menor.
+select nome,count(id_usuario) as total_Emprestimos from usuarios as u join Emprestimos as e on u.id = e.id_usuario group by nome order by count(id_usuario) desc;
+
+
